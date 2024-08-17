c7e864fd3a3cc3fe93a0d1bdced1f8c17d4df6a5
hanif <dgdfjdjf@gmail.com>
2024-08-17 13:54:36 +0000

    comment out vcn02

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ module "my_vcn01" {
   display_name   = var.vcn01_display_name
 }
 
-module "my_vcn02" {
-  source         = "./vcn_module"
-  cidr_block     = "172.16.0.0/16"
-  dns_label      = "vcn02"
-  compartment_id = var.compartment_ocid
-  display_name   = "vcn02"
-}
+# module "my_vcn02" {
+#   source         = "./vcn_module"
+#   cidr_block     = "172.16.0.0/16"
+#   dns_label      = "vcn02"
+#   compartment_id = var.compartment_ocid
+#   display_name   = "vcn02"
+# }
