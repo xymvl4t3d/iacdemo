a2ee509d063fd47890e1d4fdbe7fd13d6a101c8d
hanif <muhammadnurhanif.binabdulrashid@stengg.com>
2024-08-18 13:54:07 +0800

    Add IGW, RT, NSG and ingress rule

--- aio/02-main.tf
+++ aio/02-main.tf
@@ -1,15 +1,55 @@
 resource "oci_core_vcn" "vcn01" {
+  compartment_id = var.compartment_ocid
   cidr_block     = var.vcn_cidr_block
   display_name   = var.vcn_display_name
+}
+
+resource "oci_core_internet_gateway" "igw01" {
   compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.vcn01.id
+  display_name   = var.igw_display_name
+  enabled        = var.igw_enabled
+}
+
+resource "oci_core_route_table" "rt01" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.vcn01.id
+  display_name   = var.rt01_display_name
+
+  route_rules {
+    destination       = var.rr_dest
+    destination_type  = var.rr_dest_type
+    network_entity_id = oci_core_internet_gateway.igw01.id
+  }
 }
 
 resource "oci_core_subnet" "subnet01" {
+  compartment_id             = var.compartment_ocid
   cidr_block                 = var.subnet_cidr_block
   vcn_id                     = oci_core_vcn.vcn01.id
   display_name               = var.subnet_display_name
-  compartment_id             = var.compartment_ocid
   prohibit_public_ip_on_vnic = var.prohibit_state
+  route_table_id             = oci_core_route_table.rt01.id
+}
+
+resource "oci_core_network_security_group" "nsg01" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.vcn01.id
+  display_name   = var.nsg01_display_name
+}
+
+resource "oci_core_network_security_group_security_rule" "ir01" {
+  compartment_id            = var.compartment_ocid
+  network_security_group_id = oci_core_network_security_group.nsg01.id
+
+  security_rule {
+    direction    = var.sr_dir
+    protocol     = var.sr_prot
+    source       = var.sr_ip
+    source_type  = var.sr_st
+    is_stateless = var.sr_isl
+    desc         = var.sr_desc
+  }
 }
 
 resource "oci_core_instance" "instance01" {
@@ -20,6 +60,7 @@ resource "oci_core_instance" "instance01" {
   create_vnic_details {
     subnet_id        = oci_core_subnet.subnet01.id
     assign_public_ip = var.assign_public_ip
+    nsg_ids          = [oci_core_network_security_group.nsg01.id]
   }
 
   display_name = var.instance_display_name
--- aio/03-variables.tf
+++ aio/03-variables.tf
@@ -14,6 +14,26 @@ variable "vcn_display_name" {
   type = string
 }
 
+variable "igw_display_name" {
+  type = string
+}
+
+variable "igw_enabled" {
+  type = bool
+}
+
+variable "rt01_display_name" {
+  type = string
+}
+
+variable "rr_dest" {
+  type = string
+}
+
+variable "rr_dest_type" {
+  type = string
+}
+
 variable "subnet_cidr_block" {
   type = string
 }
@@ -24,6 +44,34 @@ variable "subnet_display_name" {
 
 variable "prohibit_state" {
   type = bool
+}
+
+variable "nsg01_display_name" {
+  type = string
+}
+
+variable "sr_dir" {
+  type = string
+}
+
+variable "sr_prot" {
+  type = string
+}
+
+variable "sr_ip" {
+  type = string
+}
+
+variable "sr_st" {
+  type = string
+}
+
+variable "sr_isl" {
+  type = bool
+}
+
+variable "sr_desc" {
+  type = string
 }
 
 variable "avail_domain" {
